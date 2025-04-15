94da4d0f8d8a66096975e32cab08d7d24699b1e1
fe4rleef <jonathan.sampson@trinitaschristian.org>
2025-04-15 19:06:27 +0000

    js doesnt work sadly '

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,38 @@
+//no changes so const
 const h1 = document.getElementById("title")
 const h2 = document.querySelector("h2")
 const el = document.querySelector("#currenttime");
 const username = prompt("what is your name dawg?")
 
+//We're going to change this value at least once
+let timevalue;
+
 // Can the user find our secret
-if ("hotdog" == prompt("Whart is the secret message?") ) {
-    h2.hidden = false; 
+let userAnswer = prompt("What is the secret message?");
+
+while (userAnswer != "hotdog" && tries > 0) {
+    alert("You made the wrong choice bucko. " + tries + "left");
+    userAnswer = prompt("what is the secret message?");
+    tries = tries - 1;
 }
+
+if (userAnswer == "hotdog") {
+    h2.hidden = false;
+}
+
 //Runs clock once every second
 setInterval(settime, 1000);
+
 //uses the name provided and greets.
 h1.textContent = "Hello, " + username
 
 function settime() {
-    el.textContent = new Date().toLocaleTimeString();
+    timevalue = new Date().toLocaleTimeString();
+    //function above uses "camel casing"
+    renderTime();
+}
 
+function renderTime() {
+   el.textContent = timevalue;
 }
 
