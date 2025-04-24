448d1f5f0bf380d332fbcff90ec27f8a6a3dc490
fe4rleef <jonathan.sampson@trinitaschristian.org>
2025-04-24 19:06:50 +0000

    created arrays that show our names ages and initials. singles out people over 15 (sorry not sorry brandon)

--- js/arrays/arrays.js
+++ js/arrays/arrays.js
@@ -13,6 +13,32 @@ function printStudentList() {
     }
 }
 
-printStudentList();
+//There are a couple types of function syntaxes we can use:
+//1. the traditional style
+// function (student) {return student [2] >= 15}
+//2. the newer array function style:
+// (student) => { return student[2] >= 15}
+//3. Array functions with implicit returns
+//  (student) => student[2] >= 15
+//4. Array function with shorter argument names
+// (s) => s[2] >= 15
+
+//Let's find all students that are 15 or older
+const fifteenPlus = students.filter((s) => s[2] >= 15);
+//In studentInitials we grab the first letter from the first/last names and return
+const studentInitials = students.map((student) => student[0][0] + student[1][0] );
+const futureAges = fifteenPlus.map((student) => 10 + student[2]);
+
+//  for ( const student of students ) {
+//     //Zero-based index. 0 has the value of 1 and so on.
+//     const age = student[2];
+//     if ( age >= 15) {
+//         fifteenPlus.push(student);  
+//     }
+// }
+
+console.log(fifteenPlus, studentInitials, futureAges);
+
+// printStudentList();
 
 //Arrays are marked by []
